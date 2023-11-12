5d1b37bbd58df19d392b6712aa4f3c4d66ba509e
sangxxjin <sj_0368@naver.com>
2023-11-12 16:08:04 +0900

    refactor: 메소드 및 메시지 OutputView로 이동

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -4,20 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-  private final static String OPENING = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
   private final static String VISIT_DATE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
   private final static String MENU = "주문하실 메뉴를 메뉴와 개수를 알려주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1";
-
-  public static void opening() {
-    System.out.println(OPENING);
-  }
 
   public static String readDate() {
     System.out.println(VISIT_DATE);
     return Console.readLine();
   }
 
-  public static String readMenu(){
+  public static String readMenu() {
     System.out.println(MENU);
     return Console.readLine();
   }
