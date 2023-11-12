6d18afacf8e8b2e93727ce0e01b9ea9736914497
sangxxjin <sj_0368@naver.com>
2023-11-12 16:19:40 +0900

    feat: 미리보기 메시지 상수 처리 및 출력 메소드 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -3,9 +3,14 @@ package christmas.view;
 public class OutputView {
 
   private final static String OPENING = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+  private final static String EVENT_PREVIEW = "12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
 
   public static void opening() {
     System.out.println(OPENING);
+  }
+
+  public static void previewEventBenefits() {
+    System.out.println(EVENT_PREVIEW);
   }
 
 }
