d5b87939de5ace92ca9e45e65655a20a98106830
sangxxjin <sj_0368@naver.com>
2023-11-12 16:03:52 +0900

    feat: 오프닝 메시지 구현 및 메시지 상수처리

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -4,7 +4,12 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
+  private final static String OPENING = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
   private final static String VISIT_DATE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+
+  public static void opening() {
+    System.out.println(OPENING);
+  }
 
   public static String readDate() {
     System.out.println(VISIT_DATE);
