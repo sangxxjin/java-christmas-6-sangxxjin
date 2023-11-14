5d0e8005f7507fa39b1e5ecec0477ccb86dc0e50
sangxxjin <sj_0368@naver.com>
2023-11-14 15:22:57 +0900

    feat: 예외 처리 구현 및 에러메시지 상수표현

--- src/main/java/christmas/Date.java
+++ src/main/java/christmas/Date.java
@@ -6,11 +6,19 @@ import java.time.LocalDate;
 public class Date {
 
   private static LocalDate date;
-
+  private static final String INVALID_DATE_MESSAGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
 
   public Date(String dateString) {
-    int parsedDate = Integer.parseInt(dateString);
-    this.date = LocalDate.of(2023, 12, parsedDate);
+    try {
+      int parsedDate = Integer.parseInt(dateString);
+      if (isValidDate(parsedDate)) {
+        this.date = LocalDate.of(2023,12,parsedDate);
+      } else {
+        throw new IllegalArgumentException(INVALID_DATE_MESSAGE);
+      }
+    } catch (NumberFormatException e) {
+      throw new IllegalArgumentException(INVALID_DATE_MESSAGE);
+    }
   }
 
   private boolean isValidDate(int date) {
