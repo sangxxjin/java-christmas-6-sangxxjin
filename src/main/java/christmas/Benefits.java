42a0b19d229080875b5675d5b5f14b29e621f149
sangxxjin <sj_0368@naver.com>
2023-11-14 15:31:46 +0900

    feat: 평일 할인, 주말 할인 메소드 구현

--- src/main/java/christmas/Benefits.java
+++ src/main/java/christmas/Benefits.java
@@ -16,6 +16,7 @@ public class Benefits {
     calculateTotalPrice(Orders.getOrders());
     selectGift();
     christmasDiscount();
+    selectDayDiscount(Orders.getOrders());
   }
 
   private void calculateTotalPrice(Map<MenuItem, Integer> orders) {
@@ -40,6 +41,22 @@ public class Benefits {
     }
     else benefitsList.add(0);
     benefits += benefitsList.get(0);
+  }
+
+  private void selectDayDiscount(Map<MenuItem, Integer> orders) {
+    int dayDiscount = 0;
+    if (Date.isFridayOrSaturday(Date.getDate())) {
+      for (Map.Entry<MenuItem, Integer> entry : orders.entrySet()) {
+        if ("메인".equals(entry.getKey().getCategory())) dayDiscount += 2303;
+      }
+    }
+    if (!Date.isFridayOrSaturday(Date.getDate())) {
+      for (Map.Entry<MenuItem, Integer> entry : orders.entrySet()) {
+        if ("디저트".equals(entry.getKey().getCategory())) dayDiscount += 2303;
+      }
+    }
+    benefitsList.add(dayDiscount);
+    benefits += benefitsList.get(1);
   }
 
   public static int getTotalPrice() {
