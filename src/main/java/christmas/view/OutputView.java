6ac70eeb6d98abf229154d83db887ade1df1990d
sangxxjin <sj_0368@naver.com>
2023-11-14 14:33:55 +0900

    feat: 할인 전 총 주문금액 출력 및 상수표현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -8,6 +8,7 @@ public class OutputView {
   private final static String OPENING = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
   private final static String EVENT_PREVIEW = " 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
   private final static String ORDER_MENU_HEADER = "<주문 메뉴>";
+  private final static String TOTAL_ORDER_PRICE_BEFORE_DISCOUNT = "<할인 전 총주문 금액>";
 
   public static void opening() {
     System.out.println(OPENING);
@@ -22,5 +23,9 @@ public class OutputView {
       System.out.println(entry.getKey().getName() + " " + entry.getValue() + "개");
     }
     System.out.println();
+  }
+  public static void totalPrice(int totalPrice){
+    System.out.println(TOTAL_ORDER_PRICE_BEFORE_DISCOUNT);
+    System.out.println(totalPrice+"원"+"\n");
   }
 }
