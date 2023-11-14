29a233efa4ab7f72f2bbcdb6d420f2d0a8ea7cd2
sangxxjin <sj_0368@naver.com>
2023-11-14 15:08:54 +0900

    feat: 주문 내역을 저장할 객체 생성

---
+++ src/main/java/christmas/Orders.java
@@ -0,0 +1,14 @@
+package christmas;
+
+import christmas.enums.MenuItem;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Orders {
+
+  private static Map<MenuItem, Integer> orders = new HashMap<>();
+
+  public Orders(String orderString) {
+  }
+
+}
