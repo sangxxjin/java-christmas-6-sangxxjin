c10ce573fac70c22dd446d89a8acb9fc7d13a27a
sangxxjin <sj_0368@naver.com>
2023-11-14 15:10:49 +0900

    feat: 예외 검증 및 예외처리

--- src/main/java/christmas/Orders.java
+++ src/main/java/christmas/Orders.java
@@ -11,4 +11,41 @@ public class Orders {
   public Orders(String orderString) {
   }
 
+  private void processOrderItem(String item, Map<MenuItem, Integer> processedOrders) {
+    String[] itemDetails = item.split("-");
+
+    if (itemDetails.length != 2) {
+      handleInvalidOrder("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    }
+
+    String itemName = itemDetails[0];
+    int itemCount = 0;
+
+    try {
+      itemCount = Integer.parseInt(itemDetails[1]);
+    } catch (NumberFormatException e) {
+      handleInvalidOrder("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    }
+
+    if (itemCount < 1) {
+      handleInvalidOrder("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    }
+
+    MenuItem menuItem = findMenuItem(itemName);
+
+    if (menuItem == null) {
+      handleInvalidOrder("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    }
+
+    if (processedOrders.containsKey(menuItem)) {
+      handleInvalidOrder("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    } else {
+      processedOrders.put(menuItem, itemCount);
+    }
+  }
+  private void handleInvalidOrder(String errorMessage) {
+    throw new IllegalArgumentException(errorMessage);
+  }
+
+
 }
