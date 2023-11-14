065739c3695576a6f7d5e563aa5c648bf991f93e
sangxxjin <sj_0368@naver.com>
2023-11-14 14:33:01 +0900

    feat: 메뉴 리스트 출력 및 상수표현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,16 +1,26 @@
 package christmas.view;
 
+import christmas.enums.MenuItem;
+import java.util.Map;
+
 public class OutputView {
 
   private final static String OPENING = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
   private final static String EVENT_PREVIEW = " 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
+  private final static String ORDER_MENU_HEADER = "<주문 메뉴>";
 
   public static void opening() {
     System.out.println(OPENING);
   }
 
   public static void previewEventBenefits(int date) {
-    System.out.println("12월 "+date+"일에"+EVENT_PREVIEW);
+    System.out.println("12월 " + date + "일에" + EVENT_PREVIEW);
   }
-
+  public static void printOrderedMenu(Map<MenuItem,Integer> orders) {
+    System.out.println(ORDER_MENU_HEADER);
+    for (Map.Entry<MenuItem, Integer> entry : orders.entrySet()) {
+      System.out.println(entry.getKey().getName() + " " + entry.getValue() + "개");
+    }
+    System.out.println();
+  }
 }
