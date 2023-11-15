2d4de7a90598580a52f9578fb0f1fc114051c3ec
sangxxjin <sj_0368@naver.com>
2023-11-15 19:14:16 +0900

    rafactor: 컨트롤러 수정 적용, 불필요한 'static' 키워드 제거, 메소드 return 방식으로 반환값을 주게 변환

--- src/main/java/christmas/model/Benefits.java
+++ src/main/java/christmas/model/Benefits.java
@@ -7,18 +7,19 @@ import java.util.List;
 import java.util.Map;
 
 public class Benefits {
-  private static List<Integer> benefitsList = new ArrayList<>();
-  private static int benefits;
+  private List<Integer> benefitsList = new ArrayList<>();
+  private int benefits;
   private static final int CHRISTMAS_EVENT_START_DATE = 1;
   private static final int CHRISTMAS_EVENT_END_DATE = 25;
   private static final int WEEK_DISCOUNT_PRICE = 2023;
   private static final int SPECIAL_DISCOUNT_PRICE = 1000;
 
-  public Benefits() {
-    christmasDiscount();
-    selectDayDiscount(Orders.getOrders());
-    specialDiscount();
-    promotion(Prices.getTotalPrice());
+  public Benefits(Prices prices, Orders orders, Date date) {
+    benefitsList.add(christmasDiscount(date));
+    benefitsList.add(selectDayDiscount(orders.getOrders(),date));
+    benefitsList.add(specialDiscount(date));
+    benefitsList.add(promotion(prices.getTotalPrice()));
+    addBenefits(benefitsList);
   }
 
   public boolean selectPromotion(int totalPrice) {
@@ -28,45 +29,44 @@ public class Benefits {
     return false;
   }
 
-  private void christmasDiscount() {
-    if (Date.getDate().getDayOfMonth() >= CHRISTMAS_EVENT_START_DATE
-        && Date.getDate().getDayOfMonth() <= CHRISTMAS_EVENT_END_DATE) {
-      benefitsList.add(900 + (Date.getDate().getDayOfMonth() * 100));
-    } else {
-      benefitsList.add(0);
+  private int christmasDiscount(Date date) {
+    int christmasDiscount=0;
+    if (date.getDate().getDayOfMonth() >= CHRISTMAS_EVENT_START_DATE
+        && date.getDate().getDayOfMonth() <= CHRISTMAS_EVENT_END_DATE) {
+      christmasDiscount = 900 + (date.getDate().getDayOfMonth() * 100);
     }
-    benefits += benefitsList.get(0);
+    return christmasDiscount;
   }
 
-  private void selectDayDiscount(Map<MenuItem, Integer> orders) {
+  private int selectDayDiscount(Map<MenuItem, Integer> orders,Date date) {
     int dayDiscount = orders.entrySet()
         .stream()
         .filter(entry ->
-            (Date.isFridayOrSaturday(Date.getDate()) && "메인".equals(entry.getKey().getCategory()))
-                || (!Date.isFridayOrSaturday(Date.getDate()) && "디저트".equals(
+            (date.isFridayOrSaturday(date.getDate()) && "메인".equals(entry.getKey().getCategory()))
+                || (!date.isFridayOrSaturday(date.getDate()) && "디저트".equals(
                 entry.getKey().getCategory())))
         .mapToInt(entry -> WEEK_DISCOUNT_PRICE * entry.getValue())
         .sum();
-
-    benefitsList.add(dayDiscount);
-    benefits += benefitsList.get(1);
+    return dayDiscount;
   }
 
-  private void specialDiscount() {
-    if (Date.isSpecialDay(Date.getDate())) {
-      benefitsList.add(SPECIAL_DISCOUNT_PRICE);
-    } else {
-      benefitsList.add(0);
+  private int specialDiscount(Date date) {
+    if (Date.isSpecialDay(date.getDate())) {
+      return SPECIAL_DISCOUNT_PRICE;
     }
-    benefits += benefitsList.get(2);
+    return 0;
   }
 
-  private void promotion(int totalPrices) {
+  private int promotion(int totalPrices) {
     if (totalPrices>120000) {
-      benefitsList.add(25000);
-      benefits += benefitsList.get(3);
-    } else {
-      benefitsList.add(0);
+      return 25000;
+    }
+    return 0;
+  }
+
+  private void addBenefits(List<Integer> benefitsList){
+    for (int i = 0; i < benefitsList.size(); i++) {
+      benefits+=benefitsList.get(i);
     }
   }
 
@@ -75,11 +75,11 @@ public class Benefits {
   }
 
 
-  public static List<Integer> getBenefitsList() {
+  public List<Integer> getBenefitsList() {
     return benefitsList;
   }
 
-  public static int getBenefits() {
+  public int getBenefits() {
     return benefits;
   }
 }
