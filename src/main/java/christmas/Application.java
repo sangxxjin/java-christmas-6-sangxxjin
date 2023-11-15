10f13ba07f586d948b7c456e4f5f7741d59f2acf
sangxxjin <sj_0368@naver.com>
2023-11-15 17:55:56 +0900

    rafactor: 컨벤션 적용

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,10 +1,12 @@
 package christmas;
 
+import christmas.controller.ControllerService;
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        ControllerService.dateRun();
-        ControllerService.ordersRun();
-        ControllerService.benefitsRun();
-    }
+
+  public static void main(String[] args) {
+    // TODO: 프로그램 구현
+    ControllerService.dateRun();
+    ControllerService.ordersRun();
+    ControllerService.benefitsRun();
+  }
 }
