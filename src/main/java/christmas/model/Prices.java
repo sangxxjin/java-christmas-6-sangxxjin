9c658b01451bf8af4af0bb2b108d944eca735aec
sangxxjin <sj_0368@naver.com>
2023-11-15 17:50:41 +0900

    rafactor: benefits에 있던 메소드 이동

--- src/main/java/christmas/model/Prices.java
+++ src/main/java/christmas/model/Prices.java
@@ -8,7 +8,14 @@ public class Prices {
   private static int totalPrice;
 
   public Prices(Map<MenuItem, Integer> orders) {
-
+    calculateTotalPrice(orders);
+  }
+  private void calculateTotalPrice(Map<MenuItem, Integer> orders) {
+    for (Map.Entry<MenuItem, Integer> entry : orders.entrySet()) {
+      MenuItem menuItem = entry.getKey();
+      int itemCount = entry.getValue();
+      totalPrice += menuItem.getPrice() * itemCount;
+    }
   }
 
   public static int getTotalPrice() {
